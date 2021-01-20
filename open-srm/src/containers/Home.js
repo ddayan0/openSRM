66be66fd465fdcbf9aba3b8cc4299cc0fe7bbd19
ddayan0 <denday198@gmail.com>
2021-01-20 12:13:26 -0500

    got router working, terminal ready, getting things finalized

---
+++ open-srm/src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Terminal from '../components/Terminal'
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <Terminal></Terminal>
+            </div>
+        )
+    };
+};
+
+export default Home;
